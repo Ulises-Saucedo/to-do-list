0c17bedbd11f4fc793805ee3cf0aa4367bba89e3
Ulises Saucedo <ulisesaucedo.se04@gmail.com>
2024-01-06 22:06:06 -0300

    styles improvement

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -5,41 +5,43 @@
         <input type="text" class="form-control" id="inputAddress2" placeholder="Agregue sus tareas" v-model="task">
       </div>
       <div class="col-2">
-        <button class="btn btn-primary w-100" @click="addTask">Agregar tarea</button>
+        <button class="btn btn-primary w-100" @click="addTask">
+          <v-icon name="io-add-circle-outline"></v-icon>
+        </button>
       </div>
     </div>
-    <table class="table table-striped table-hover">
-      <thead>
-        <tr>
-          <th scope="col">Tareas</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(tarea, i) in tasks" :key="i" :class="{ 'table-success': tarea.isSelected }" @dblclick="toggleSelection(i)">
-          <td class="col-10">{{ tarea.title }}</td>
-          <td class="col-2 d-flex justify-content-end align-items-center">
-            <button class="btn btn-danger me-3" @click="removeTask(i)">Eliminar</button>
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop" @click="editTask(i)">Editar</button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div scope="col">Tareas</div>
+    <div v-for="(tarea, i) in tasks" :key="i" class="flex justify-between my-3 shadow p-1 rounded bg-gradient-to-r from-red-200 via-red-300 to-yellow-200">
+      <div class="p-1 font-semibold">{{ tarea.title }}</div>
+      <div>
+        <button class="btn shadow-inner hover:bg-red-500 mx-1" @click="removeTask(i)">
+          <v-icon name="md-deleteoutline"></v-icon>
+        </button>
+        <button type="button" class="btn shadow-inner hover:bg-sky-700 mx-1" data-bs-toggle="modal" data-bs-target="#staticBackdrop" @click="editTask(i)">
+          <v-icon name="hi-solid-pencil-alt"></v-icon>
+        </button>
+      </div>
+    </div>
   </div>
 
   <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
   <div class="modal-dialog">
-    <div class="modal-content">
+    <div class="modal-content bg-gradient-to-r from-red-200 via-red-300 to-yellow-200">
       <div class="modal-header">
         <h1 class="modal-title fs-5" id="staticBackdropLabel">Editar tarea</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+          <v-icon name="ri-delete-back-2-fill"></v-icon>
+        </button>
       </div>
       <div class="modal-body">
         <div class="row g-3 mb-3">
           <div class="col-10">
-            <input type="text" class="form-control" id="inputAddress2" placeholder="Edite el nombre de la tarea" v-model="taskModal">
+            <input type="text" class="form-control bg-none" id="inputAddress2" placeholder="Edite el nombre de la tarea" v-model="taskModal">
           </div>
           <div class="col-2">
-            <button class="btn btn-primary w-100" @click="edit()">Editar</button>
+            <button class="btn shadow-inner bg-sky-700 hover:bg-sky-500 w-100" @click="edit()">
+              <v-icon name="gi-confirmed"></v-icon>
+            </button>
           </div>
         </div>
       </div>
@@ -78,8 +80,4 @@
     taskModal.value = tasks.value[i].title 
     editingIndex = i
   }
-
-  const toggleSelection = (index) => {
-    tasks.value[index].isSelected = !tasks.value[index].isSelected;
-  };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { OhVueIcon, addIcons } from "oh-vue-icons"
+import { HiSolidPencilAlt, MdDeleteoutline, RiDeleteBack2Fill, GiConfirmed, IoAddCircleOutline } from "oh-vue-icons/icons"
+addIcons( HiSolidPencilAlt, MdDeleteoutline, RiDeleteBack2Fill, GiConfirmed, IoAddCircleOutline )
 
-createApp(App).mount('#app')
+createApp(App).component("v-icon", OhVueIcon).mount('#app')
